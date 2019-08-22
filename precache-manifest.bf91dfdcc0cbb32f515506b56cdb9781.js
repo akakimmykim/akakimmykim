70b2c14ab75708752e1cfc3d5850e3559807048a
U-DESKTOP-30LRVN7\akakimmykim <akakimmykim@gmail.com>
2019-08-21 22:23:24 -0400

    fixed files

---
+++ precache-manifest.bf91dfdcc0cbb32f515506b56cdb9781.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a0ad3ab1a897662333e3b74711da9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25da4e200a5436958981",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5b513593dcc2a64e6e0d",
+    "url": "/static/css/main.cb37767c.chunk.css"
+  },
+  {
+    "revision": "25da4e200a5436958981",
+    "url": "/static/js/2.de4b68f9.chunk.js"
+  },
+  {
+    "revision": "5b513593dcc2a64e6e0d",
+    "url": "/static/js/main.daf60ec9.chunk.js"
+  },
+  {
+    "revision": "737425d3abf597188c5e",
+    "url": "/static/js/runtime~main.46ff90e1.js"
+  },
+  {
+    "revision": "19feaf1435e0b4522f2809f3938bbb42",
+    "url": "/static/media/Attention Grabber with Photo.19feaf14.gif"
+  },
+  {
+    "revision": "1e68c5714ca88b6656905750a8ce5c00",
+    "url": "/static/media/AttentionGrabberMobile.1e68c571.gif"
+  },
+  {
+    "revision": "49a18285e6b0ef14818e46b65125b66b",
+    "url": "/static/media/AttentionGrabberMobileSmaller.49a18285.gif"
+  },
+  {
+    "revision": "89a48016a35de40491356ac507fff4f9",
+    "url": "/static/media/Awards.89a48016.gif"
+  },
+  {
+    "revision": "cff6a7b803d1cba80e9cf55ea9060f5b",
+    "url": "/static/media/Blog.cff6a7b8.gif"
+  },
+  {
+    "revision": "8ffd3c315ca1b33f7a738d7f64d9e1a7",
+    "url": "/static/media/Blueprint Table.8ffd3c31.jpg"
+  },
+  {
+    "revision": "476b165681cd29dd217426e8eb66f991",
+    "url": "/static/media/Contact.476b1656.gif"
+  },
+  {
+    "revision": "411638b493fa646bf323a0b19af2c66f",
+    "url": "/static/media/DANIME.411638b4.gif"
+  },
+  {
+    "revision": "3e759f28349a4c272722d29d0da28f05",
+    "url": "/static/media/GithubQRCode.3e759f28.svg"
+  },
+  {
+    "revision": "5733bb0fb29950b9a937de04081efeb1",
+    "url": "/static/media/Logo 200px.5733bb0f.jpg"
+  },
+  {
+    "revision": "9847a13ffe23bcfa8788dc33227d8ee8",
+    "url": "/static/media/Logo 400px.9847a13f.png"
+  },
+  {
+    "revision": "ad977211b8e4f1a0b4ac7f3cc5c0f9ca",
+    "url": "/static/media/Logo 400px.ad977211.jpg"
+  },
+  {
+    "revision": "4694b06e104f28575d9c86d2405fc7bf",
+    "url": "/static/media/Logo 500px.4694b06e.jpg"
+  },
+  {
+    "revision": "f0a4504e186cc0e45acc14bcb33a506f",
+    "url": "/static/media/Picture.f0a4504e.gif"
+  },
+  {
+    "revision": "97a526b273062b1f06c061195b5e45a7",
+    "url": "/static/media/Projects.97a526b2.gif"
+  },
+  {
+    "revision": "c109d0f23160f1f2ad9be2f1a90dca5c",
+    "url": "/static/media/Smaller Portrait.c109d0f2.jpg"
+  },
+  {
+    "revision": "6626885931e951b4eab6d9025d8a66bb",
+    "url": "/static/media/TableMobile.66268859.png"
+  },
+  {
+    "revision": "c4c95bf275ba1c125c52c844552da0b8",
+    "url": "/static/media/TableMobileSmaller.c4c95bf2.jpg"
+  },
+  {
+    "revision": "46be51781b57e4ab9ddbd49dca9b9f72",
+    "url": "/static/media/Teaching.46be5178.gif"
+  },
+  {
+    "revision": "aa364dc1181508d79d6ef8a5ce1759de",
+    "url": "/static/media/WhatsappQRCode.aa364dc1.svg"
+  },
+  {
+    "revision": "03a716c30571dbb29b10621d4a04ac39",
+    "url": "/static/media/adjustable barbed bolt.03a716c3.JPG"
+  },
+  {
+    "revision": "a9b4f2955bf1b13ada38a567bc793ed7",
+    "url": "/static/media/alternativeBECCS.a9b4f295.JPG"
+  },
+  {
+    "revision": "729f5cc45a2b07b785185b4c50b65b21",
+    "url": "/static/media/biology.729f5cc4.gif"
+  },
+  {
+    "revision": "24471809dbeed3464bedcae37d686067",
+    "url": "/static/media/casualPortrait.24471809.jpg"
+  },
+  {
+    "revision": "be68e8b16242ac09207cade0f9165b94",
+    "url": "/static/media/chemistry.be68e8b1.gif"
+  },
+  {
+    "revision": "9d6ac54cb59b88657fe5397dd38d7acb",
+    "url": "/static/media/dalat.9d6ac54c.jpg"
+  },
+  {
+    "revision": "9b0a317914ee204c35a07a231300ac89",
+    "url": "/static/media/earcos.9b0a3179.jpg"
+  },
+  {
+    "revision": "8a6cc3c42958489ee3371c5c3bd1a43f",
+    "url": "/static/media/english.8a6cc3c4.gif"
+  },
+  {
+    "revision": "fd97a43b5f59f683e93b3e7f069c2f1c",
+    "url": "/static/media/fishTree.fd97a43b.gif"
+  },
+  {
+    "revision": "c83fe911f68d5288993fd4772c05fc4a",
+    "url": "/static/media/gmailQRCode.c83fe911.svg"
+  },
+  {
+    "revision": "f366d935feabce9497398214cf7f96e5",
+    "url": "/static/media/linkedInQRCode.f366d935.svg"
+  },
+  {
+    "revision": "7a864b5e2a2f0482626782c9eeebafa7",
+    "url": "/static/media/messengerQRCode.7a864b5e.svg"
+  },
+  {
+    "revision": "5218dd6b5d9e816b12f12463f43219db",
+    "url": "/static/media/metalorganicframeworks.5218dd6b.JPG"
+  },
+  {
+    "revision": "c109d0f23160f1f2ad9be2f1a90dca5c",
+    "url": "/static/media/professionalPortrait.c109d0f2.jpg"
+  },
+  {
+    "revision": "9b9375763e33c5767bb1ab071231caad",
+    "url": "/static/media/semi-professional photo.9b937576.jpg"
+  },
+  {
+    "revision": "1c8eaaf3ce809794dd86bc79f795c5e6",
+    "url": "/static/media/signature.1c8eaaf3.jpg"
+  },
+  {
+    "revision": "ef008e814f724db69cb8a827b2e4d699",
+    "url": "/static/media/study.ef008e81.gif"
+  }
+]);
